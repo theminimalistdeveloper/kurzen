b04329455956b4c0fd4404e8e0e59817bd0ecbde
Rafael Dias <theminimalistdeveloper@gmail.com>
2022-06-11 15:45:30 +0200

    fix: typo key field name

--- src/lib/persistence.ts
+++ src/lib/persistence.ts
@@ -4,27 +4,28 @@ import {
   GetItemCommandInput,
 } from '@aws-sdk/client-dynamodb'
 
-const { TABLE_NAME, REGION } = process.env
+const { TABLE_NAME } = process.env
 
 export type Link = {
   url: string
   slug: string
 }
 export const getLink = async (slug: string): Promise<Link | null> => {
-  const client = new DynamoDBClient({ region: REGION })
+  const client = new DynamoDBClient({})
   const input: GetItemCommandInput = {
     TableName: TABLE_NAME,
     Key: {
       Slug: { S: slug },
     },
   }
+
   const command = new GetItemCommand(input)
   const link = await client.send(command)
 
   return link.Item
     ? {
         url: link.Item.url.S as string,
-        slug: link.Item.slug.S as string,
+        slug: link.Item.Slug.S as string,
       }
     : null
 }
